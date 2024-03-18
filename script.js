f20e1115a50c5d05ac25251d2faf005ccd2bde3b
NaveenPensia70 <naveenpensia07@gmail.com>
2024-03-18 11:00:47 +0530

    logic for win-lose is created

--- script.js
+++ script.js
@@ -13,9 +13,45 @@ function getComputerChoice()
     }
 }
 const computer_selection=getComputerChoice();
-console.log(player_selection);
-console.log(computer_selection);
-function playground()
+// console.log(player_selection);
+// console.log(computer_selection);
+let computer_win=0;
+let player_win=0;
+function playground(player_selection,computer_selection)
 {
+    if(player_selection=='rock')
+    {
+        if(computer_selection==='paper')
+        {
+            console.log(++computer_win);
+        }
+        else if(computer_selection='scissors')
+        {
+            console.log(++player_win);
+        }
+    }
+    if(player_selection=='paper')
+    {
+        if(computer_selection==='scissors')
+        {
+            console.log(++computer_win);
+        }
+        else if(computer_selection='rock')
+        {
+            console.log(++player_win);
+        }
+    }
+    if(player_selection=='scissors')
+    {
+        if(computer_selection==='rock')
+        {
+            console.log(++computer_win);
+        }
+        else if(computer_selection='paper')
+        {
+            console.log(++player_win);
+        }
+    }
 
 }
+playground(player_selection,computer_selection);
